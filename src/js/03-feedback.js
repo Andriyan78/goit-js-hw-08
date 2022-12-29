805ffa1730c2fe28b3a1ac01a945a397015d483d
Andriyan78 <110392316+Andriyan78@users.noreply.github.com>
2022-12-29 07:22:09 +0200

    last

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -12,21 +12,24 @@ onPageLoad();
 
 function onFormSubmit(evt) {
   evt.preventDefault();
-  evt.currentTarget.reset();
   const email = evt.target.elements.email.value;
-  const password = evt.target.elements.password.value;
-
-  if (email !== '' || password !== '') {
-  console.dir(JSON.parse(localStorage.getItem(LOCAL)));
-  localStorage.removeItem(LOCAL);
-  formData = {};
-    };
+  const message = evt.target.elements.message.value;
+  if (email === '' || message === '') {
+    alert ("fill all the fields")
+  } else {
+    console.dir(JSON.parse(localStorage.getItem(LOCAL)));
+    localStorage.removeItem(LOCAL);
+    formData = {};  
+  }
+  
+  evt.currentTarget.reset();  
 }
 
 function onFormInput(evt) {
   formData[evt.target.name] = evt.target.value;
   localStorage.setItem(LOCAL, JSON.stringify(formData));
 }
+ 
 
 function onPageLoad() {
   const savedData = JSON.parse(localStorage.getItem(LOCAL));
